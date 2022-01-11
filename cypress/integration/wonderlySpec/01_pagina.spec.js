e31d85f05227972f67cabf87ad4a6969dad53a40
Yanina <yani.cardozosalas@gmail.com>
2022-01-11 16:25:32 -0400

    codigo subido de navegacion

--- cypress/integration/wonderlySpec/01_pagina.spec.js
+++ cypress/integration/wonderlySpec/01_pagina.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 // Casos de prueba realizado por Patrik Delgadillo
 
+import { Destrezas } from "../../paginas/Destrezas";
 import { Home } from "../../paginas/Home";
 import { Ingresar } from "../../paginas/Ingresar";
 import { Perfil } from "../../paginas/Perfil";
@@ -13,6 +14,7 @@ describe("Home Page - UI", () => {
   const home = new Home();
   const ingresar = new Ingresar();
   const perfil = new Perfil();
+  const destrezas = new Destrezas();
 
   beforeEach(() => {
     cy.visit("https://developers.learnwonderly.com/");
@@ -75,5 +77,77 @@ describe("Home Page - UI", () => {
     home.click_Membresia_Explorador();
     cy.wait(3000);
     cy.get(".my-5").should("be.visible");
+  });
+
+  // Navegacion Menu Abajo
+
+  it("Comprobar que el usuario se encuentre en la pestaña de Impacto social", () => {
+    home.impactoSocial();
+    cy.get("h2").should("be.visible");
+  });
+
+  it("Verificar que el usuario se encuentre en la materia Robotica desde el menu Destrezas", () => {
+    home.menu_Robotica();
+    cy.get("h2.mb-4").should("contain.text", "Robótica");
+  });
+
+  it("Verificar que el usuario  se encuentre en la materia Programacion desde el menu Destrezas", () => {
+    home.menu_Programacion();
+    cy.get("h2.mb-4")
+      .should("contain.text", "Programación")
+      .should("be.visible");
+  });
+
+  it("Verificar que el usuario  se encuentre en la materia Desarrollo Video Juegos desde el menu Destrezas", () => {
+    home.menu_DesarrolloVideoJuegos();
+    cy.get("h2.mb-4")
+      .should("contain.text", "Desarrollo de videojuegos")
+      .should("be.visible");
+  });
+
+  it("Verificar que el usuario se encuentre en la materia Matematicas desde el menu Destrezas", () => {
+    home.menu_Matematicas();
+    cy.get("h2.mb-4")
+      .should("contain.text", "Matemáticas")
+      .should("be.visible");
+  });
+
+  it("Verificar que el usuario  se encuentre en la materia Ingles desde el menu Destrezas", () => {
+    home.menu_Ingles();
+    cy.get("h2.mb-4").should("contain.text", "Inglés").should("be.visible");
+  });
+
+  // Boton "Mira el curso" desde la pestaña Destrezas menu Arriba
+
+  it("Demostrar que al presionar el boton Mira el curso redireccione al contenido de Programacion", () => {
+    home.click_Destrezas();
+    destrezas.botonMirarcursoProgramacion();
+    cy.get("h2.mb-4")
+      .should("contain.text", "Programación")
+      .should("be.visible");
+  });
+
+  it("Demostrar que al presionar el boton Mira el curso redireccione al contenido de Robotica", () => {
+    home.click_Destrezas();
+    destrezas.botonMirarCursoRobotica();
+    cy.get("h2.mb-4").should("contain.text", "Robótica");
+  });
+
+  it("Demostrar que al presionar el boton Mira el curso redireccione al contenido de Ingles", () => {
+    home.click_Destrezas();
+    destrezas.botonMirarCursoIngles();
+    cy.get("h2.mb-4").should("contain.text", "Inglés");
+  });
+
+  it("Demostrar que al presionar el boton Mira el curso redireccione al contenido de Matematicas", () => {
+    home.click_Destrezas();
+    destrezas.botonMirarCursoMatematicas();
+    cy.get("h2.mb-4").should("contain.text", "Matemáticas");
+  });
+
+  it("Demostrar que al presionar el boton Mira el curso redireccione a la Vista de desarrollo de Video Juegos", () => {
+    home.click_Destrezas();
+    destrezas.botonMirarCursoDesarrolloVideoJuego();
+    cy.get("h2.mb-4").should("contain.text", "Desarrollo de videojuegos");
   });
 });
